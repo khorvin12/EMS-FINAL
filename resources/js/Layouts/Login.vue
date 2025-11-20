9be7da8b6709d5b90cbc91634417d3cfcd0a0ed3
khorvin12 <khorvincarl876@gmail.com>
2025-11-20 09:53:43 +0800

    Add simple login page

---
+++ resources/js/Layouts/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <input type="email" v-model="email" placeholder="Email" required />
+      <input type="password" v-model="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+    </form>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      message: ""
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const res = await axios.post("/api/login", {
+          email: this.email,
+          password: this.password,
+        });
+        this.message = "Login successful!";
+      } catch (error) {
+        this.message = "Invalid email or password.";
+      }
+    },
+  },
+};
+</script>
