4509f867e9660b9b8400ac6ae479e7961995ea1a
khorvin12 <khorvincarl876@gmail.com>
2025-11-20 09:46:55 +0800

    Initial commit

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <div>
+                    <a href="/">Home</a>
+                    <a href="about">About</a>
+                </div>
+            </nav>
+        </header>
+        
+        <main>
+            <slot/>
+        </main>
+    </div>
+</template>
