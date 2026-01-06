61400216ee12800c5097910d46469af076330cec
nicomorilla <nicomorilla5324@gmail.com>
2026-01-06 15:08:21 +0800

    updated Layout and Dashboard

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,7 +1,49 @@
 <script setup>
-
+import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
+    <div class="flex flex-col h-screen ">
+        <!-- Top Bar-->
+        <header class="bg-red-800 text-white">
+            <nav class="flex items-center justify-between text-3xl p-6 font-bold">
+                <h1 class="ml-14">Admin</h1>
+                <div class="space-x-6">
+                    <Link href="/dashboard">Logout</Link>
+                </div>
+            </nav>
+        </header>
+
+        <div class="flex flex-1">
+            <!-- Side Bar -->
+            <aside class="bg-gray-800 text-white flex justify-center w-64 py-10 text-xl">
+                <nav class="space-y-8">
+                    <div class="rounded-md px-10 py-2 bg-red-700">
+                        <Link href="/dashboard"><i class="fa fa-tachometer" aria-hidden="true" />Dashboard</Link>
+                    </div>
     
+                    <div class="rounded-md p-2 text-center hover:bg-red-700">
+                        <Link href="/employees"><i class="fa fa-users" aria-hidden="true" />Employees</Link>
+                    </div>
+    
+                    <div class="rounded-md p-2 text-center hover:bg-red-700">
+                        <Link href="/departments"><i class="fa fa-building" aria-hidden="true" />Departments</Link>
+                    </div>
+    
+                    <div class="rounded-md p-2 text-center hover:bg-red-700">
+                        <Link href="/leaves"><i class="fa fa-calendar" aria-hidden="true" />Leaves</Link>
+                    </div>
+    
+                    <div class="rounded-md p-2 text-center hover:bg-red-700">
+                        <Link href="/settings"><i class="fa fa-cog" aria-hidden="true" />Settings</Link>
+                    </div>
+                </nav>
+            </aside>
+
+            <!-- Main Content -->
+            <main class="flex-1 overflow-y-auto bg-slate-200 p-8">
+                <slot />
+            </main>
+        </div>
+    </div>
 </template>
