acb92a4712c0910778d686b4066ebd14798bfb57
nicomorilla <nicomorilla5324@gmail.com>
2026-01-08 15:24:51 +0800

    Added Admin Dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,21 @@
 import './bootstrap';
 import '../css/app.css';
+
 import { createApp, h } from 'vue'
 import { createInertiaApp, Link } from '@inertiajs/vue3'
+import Layout from './Layouts/AdminLayout.vue';
 
 createInertiaApp({
   resolve: name => {
-    const pages = import.meta.glob('./Layouts/**/*.vue', { eager: true })
-    return pages[`./Layouts/${name}.vue`]
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
+    let page = pages[`./Pages/${name}.vue`];
+    page.default.layout = page.default.layout || Layout;
+    return page;
   },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
-      .component("Link", Link)
+      .component('Link', Link)
       .mount(el)
   },
 })
