4b0f922a82d4b822c9eb62defd93557d265f5813
nicomorilla <nicomorilla5324@gmail.com>
2026-01-08 16:01:26 +0800

    Added AddNewDepartment

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@ Route::inertia('/admindashboard', 'Admin/AdminDashboard')->name('admindashboard'
 
 Route::inertia('/managedepartment', 'Admin/Departments/ManageDepartment')->name('managedepartment');
 Route::post('/managedepartment', [AuthController::class, 'managedepartment']);
+
+Route::inertia('/adddepartment', 'Admin/Departments/AddDepartment')->name('adddepartment');
+Route::post('/adddepartment', [AuthController::class, 'adddepartment']);
