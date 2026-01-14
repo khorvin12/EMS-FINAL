f9bb855b537187bbf7e53173e037a4ada5f9431f
nicomorilla <nicomorilla5324@gmail.com>
2026-01-14 14:12:08 +0800

    Added Employee Layout

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Home');
-});
+Route::inertia('/index', 'Employee/Index')->name('index');
