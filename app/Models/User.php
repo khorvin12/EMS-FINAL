0034107f3f7a354cf3e34b595ec726de570f7b80
khorvin12 <khorvincarl876@gmail.com>
2026-01-19 14:37:12 +0800

    List-Employee

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,21 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -33,21 +26,11 @@ class User extends Authenticatable
         'salary',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -67,11 +50,5 @@ class User extends Authenticatable
         return $this->belongsTo(Department::class);
     }
 
-    /**
-     * Get the role that the user belongs to.
-     */
-    public function roleRelation()
-    {
-        return $this->belongsTo(Role::class, 'role_id');
-    }
+    // Remove roleRelation() method since Role model doesn't exist
 }
