70df1fbd9cf19e04b874877e0dd8c6c63054332b
nicomorilla <nicomorilla5324@gmail.com>
2026-01-21 10:26:21 +0800

    Describe what you changed

---
+++ resources/js/Pages/Employee/Attendance/Index.vue
@@ -0,0 +1,85 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <h1 class="text-center text-3xl font-bold mb-12">Your Attendance</h1>
+
+    <div class="flex justify-between mb-6">
+        <div class="bg-white rounded-md p-2">
+            <input type="search" placeholder="Search By SNO" />
+        </div>
+    </div>
+
+    <table class="w-full shadow-lg overflow-hidden table-fixed bg-white rounded-lg">
+        <thead class="border-slate-200 text-slate-600">
+            <tr class="bg-gray-400 text-black font-medium">
+                <th class="">SNO</th>
+                <th class="p-6">Employee ID</th>
+                <th class="">Date</th>
+                <th class="">Time In</th>
+                <th class="">Time Out</th>
+                <th class="">Hours</th>
+                <th class="">Status</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr class="bg-white-100 text-center border-slate-200 border-t-4">
+                <td class="p-4">1</td>
+                <td class="p-4">11111</td>
+                <td class="p-4">1/30/2026</td>
+                <td class="p-4">8:00 AM</td>
+                <td class="p-4">5:00 PM</td>
+                <td class="p-4">9</td>
+                <td class="p-4">
+                    <button class="bg-green-500 rounded-sm px-2 py-1">Present</button>
+                </td>
+            </tr>
+            <tr class="bg-white-100 text-center border-slate-200 border-t-4">
+                <td class="p-4">2</td>
+                <td class="p-4">22222</td>
+                <td class="p-4">2/20/2026</td>
+                <td class="p-4">9:00 AM</td>
+                <td class="p-4">5:00 PM</td>
+                <td class="p-4">8</td>
+                <td class="p-4">
+                    <button class="bg-yellow-300 rounded-sm px-2 py-1">Late</button>
+                </td>
+            </tr>
+            <tr class="bg-white-100 text-center border-slate-200 border-t-4">
+                <td class="p-4">3</td>
+                <td class="p-4">33333</td>
+                <td class="p-4">3/10/2026</td>
+                <td class="p-4">--</td>
+                <td class="p-4">--</td>
+                <td class="p-4">0</td>
+                <td class="p-4">
+                    <button class="bg-red-500 rounded-sm px-2 py-1">Absent</button>
+                </td>
+            </tr>
+            <tr class="bg-white-100 text-center border-slate-200 border-t-4">
+                <td class="p-4">4</td>
+                <td class="p-4">44444</td>
+                <td class="p-4">4/20/2026</td>
+                <td class="p-4">--</td>
+                <td class="p-4">--</td>
+                <td class="p-4">0</td>
+                <td class="p-4">
+                    <button class="bg-blue-400 rounded-sm px-2 py-1">On Leave</button>
+                </td>
+            </tr>
+            <tr class="bg-white-100 text-center border-slate-200 border-t-4">
+                <td class="p-4">5</td>
+                <td class="p-4">55555</td>
+                <td class="p-4">5/20/2026</td>
+                <td class="p-4">10:00 AM</td>
+                <td class="p-4">5:00 PM</td>
+                <td class="p-4">7</td>
+                <td class="p-4">
+                    <button class="bg-green-500 rounded-sm px-2 py-1">Present</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Login');
-});
+// Employee Dashboard Route
+Route::inertia('/index', 'Employee/Index')->name('index');
+
+// Employee Leave Routes
+Route::inertia('/leave', 'Employee/Leaves/Index')->name('leave');
+
+Route::inertia('/create-leave', 'Employee/Leaves/Create')->name('create-leave');
+
+Route::inertia('/view-leave', 'Employee/Leaves/View')->name('view-leave');
+
+// Employee Salary Routes
+Route::inertia('/employee-salary', 'Employee/Salary/Index')->name('employee-salary');
+
+// Employee Attendance Route
+Route::inertia('/attendance', 'Employee/Attendance/Index')->name('attendance');
+
+// Employee Settings Routes
+Route::inertia('/settings', 'Employee/Settings/Index')->name('settings');
