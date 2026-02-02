fb9c911c91984b64997c2d4cbc7d7253c4150360
nicomorilla <nicomorilla5324@gmail.com>
2026-02-02 15:45:51 +0800

    Added Employee Create Leave

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::inertia('/index', 'Employee/Index')->name('index');
+
+Route::inertia('/employee/leave/create', 'Employee/Leaves/Create')->name('/employee/leave/create');
