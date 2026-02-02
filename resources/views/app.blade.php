fe5a297d0436fc93fdc89067acc8115d94b4016b
nicomorilla <nicomorilla5324@gmail.com>
2026-02-02 10:45:16 +0800

    HR Layout Updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    @vite('resources/js/app.js')
-    @inertiaHead
-  </head>
-  <body>
-    @inertia
-  </body>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+    integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  @vite('resources/js/app.js')
+  @inertiaHead
+</head>
+
+<body>
+  @inertia
+</body>
+
 </html>
