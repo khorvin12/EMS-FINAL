983ec06c3fabb1c460dc4da1f7cd587cea5594a8
NIX <nicomorilla5324@gmail.com>
2026-07-12 19:15:05 +0800

    Add Employee Fix

--- resources/js/Pages/Admin/ManageEmployees/ManageEmployee.vue
+++ resources/js/Pages/Admin/ManageEmployees/ManageEmployee.vue
@@ -95,7 +95,7 @@ const tableColumns = [
           class="bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 rounded-md">
           Employee List PDF
         </a>
-        <Link href="/admin/employees/create"
+        <Link href="/admin/employee/create"
           class="bg-green-500 hover:bg-green-600 text-white font-semibold rounded-md px-4 py-2">
           Add Employee
         </Link>
